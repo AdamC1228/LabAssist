37e011aadcc25535517f681c0d84e68eeea4e628
root <root@server.lan>
2018-04-23 19:32:24 -0400

    Fix things

--- logic/reports-logic.php
+++ logic/reports-logic.php
@@ -606,14 +606,17 @@ eof;
 
 
 function reportHourlyUsage() {
+
 	$query = <<<'SQL'
 WITH dept_sections AS (
 	SELECT * FROM term_sections
 		JOIN classes ON term_sections.cid = classes.cid
 		WHERE classes.dept = ?
 )
+SELECT usage.markin, usage.markout FROM usage
+	JOIN dept_sections ON usage.secid = dept_sections.secid
 SQL;
-	$dept = getUsersDepartment($_SESSION['username'])
+	$dept = getUsersDepartment($_SESSION['username']);
 	$data = safeDBQuery($query, array($dept));
 
 	if($data === -1) {
@@ -632,7 +635,7 @@ SQL;
 	for($i = 1; $i <= 5; $i++) {
 		$retval[$i] = array();
 
-		for($j = $lstart['tm_hour']; $j <= $lend['tm_end']; $j++) {
+		for($j = $lstart['tm_hour']; $j <= $lend['tm_hour']; $j++) {
 			$retval[$i][$j] = 0;
 		}
 	}
@@ -712,7 +715,10 @@ SQL;
 
 
 function getLimits($dept) {
-	return safeDBQuery("SELECT deptlabs.labstart, deptlabs.labend FROM deptlabs WHERE deptlabs.dept = ?", array($dept));
+	$query = <<<SQL
+SELECT deptlabs.labstart, deptlabs.labend FROM deptlabs WHERE deptlabs.dept = ?
+SQL;
+	return safeDBQuery($query, array($dept));
 }
 
 function reportSectionVisits($sect) {
@@ -741,21 +747,4 @@ SQL;
 /*
     NOTE: A report on clockin length might be useful.
  */
-
-/*
- * Get the department for a user.
- */
-function getUsersDepartment($user)
-{
-	$result = databaseQuery("SELECT deptid FROM users WHERE username=?", array($user));
-
-	if(!empty($result) && is_array($result))
-	{
-		return $result[0]['deptid'];
-	}
-	else
-	{
-		return -1;
-	}
-}
 ?>

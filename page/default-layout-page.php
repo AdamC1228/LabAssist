329619a36306ec42e96d8d2e5afcf745f069d723
root <root@server.lan>
2018-05-02 19:16:02 -0400

    Display release version in the footer.

--- page/default-layout-page.php
+++ page/default-layout-page.php
@@ -1,16 +1,25 @@
 <?php
 
+require_once "logic/common/version.php";
+
 /*
  * Print the default layout for content.
  */
 function printContent($embedHTML)
 {
+    $version=getCurrentVersion();
+    
 	echo<<<eof
 	<link rel="stylesheet" type="text/css" href="styles/tables.css" />
 	<link rel="stylesheet" type="text/css" href="styles/normalize.css" />
 	<div class="content-wrapper">
 	    <div class="content">
 		$embedHTML
+		
+        <div class='footerCust'>
+            <b><em>$version</em></b>
+        </div>
+        
 	    </div>  <!-- Closing div for content -->
 	</div>  <!-- Closing div for content-wrapper -->
 eof;
